d7f591f38620e2d600f07c73251fbe9317750aa0
Rui Conceição <163507940+ruiconceicao@users.noreply.github.com>
2025-02-12 21:50:45 +0000

    Quick Start

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [LogoComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {}
